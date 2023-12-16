85fa3dfc3ed4c8cb5036c7afaa6335a2fdb35321
Abhijeet973 <98979587+abhijeet-973@users.noreply.github.com>
2023-12-16 10:59:10 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,43 @@
+const apiKey = 'AIzaSyDbli4VA6o5bsGT2hpOqSRPjJToDt6bz2A';
+const script = document.createElement('script');
+script.src = `https://www.youtube.com/iframe_api?api_key=${apiKey}`;
+document.head.appendChild(script);
+
+let player;
+
+function onPlayerReady(event) {
+  // Player is ready
+}
+
+function onPlayerStateChange(event) {
+  // Handle player state changes if needed
+}
+
+function loadVideo() {
+  const videoId =  document.getElementById('videoId').value;
+  if (videoId) {
+      player.loadVideoById(videoId);
+  } else {
+      alert('Please enter a valid YouTube Video ID.');
+  }
+}
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        playerVars: {
+            'autoplay': 0,
+            'controls': 1,
+            'showinfo': 0,
+            'rel': 0,
+            'fs': 1,
+            'modestbranding': 1,
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
